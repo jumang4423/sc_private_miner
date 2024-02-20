3a38cbefc5b5580a10bf17fbc5ca9332c6246918
jumang4423 <catontheskate4423@gmail.com>
2024-02-20 21:43:55 +0900

    big artists!

--- mining.py
+++ mining.py
@@ -8,28 +8,37 @@ import re
 import os
 
 # Constants and environment variables
-PRIVATE_URLS_FILENAME = 'private_urls.txt'
-SMALL_PLAYCOUNT_FILENAME = 'small_playcount.txt'
-SMALL_PLAYCOUNT_THRESHOLD = 30000
-NORMAL_URLS_FILENAME = 'normal_urls.txt'
+PRIVATE_SMALL_ARTIST_URLS_FILENAME = 'mining_private_small_urls.txt'
+PRIVATE_BIG_ARTIST_URLS_FILENAME = 'mining_private_big_artist_urls.txt'
+PUBLIC_SMALL_PLAYCOUNT_URLS_FILENAME = 'mining_public_small_playcount_urls.txt'
+PUBLIC_BIG_PLAYCOUNT_URLS_FILENAME = 'mining_public_big_playcount_urls.txt'
+PUBLIC_SMALL_PLAYCOUNT_THRESHOLD = 30000
+PRIVATE_BIG_ARTIST_THRESHOLD = 1000
 LOG_DIR = 'logs'
+
 # Create logs directory if it doesn't exist
 os.makedirs(LOG_DIR, exist_ok=True)
 
 # Enum of URL types
 class URLType:
-    PRIVATE = "private"
-    SMALL_PLAYCOUNT = "small_playcount"
-    NORMAL = "normal"
+    # small private track
+    PRIVATE_SMALL_ARTIST = "private_small_artist"
+    # private track with more than 1000 followers
+    PRIVATE_BIG_ARTIST = "private_big_artist"
+    # small playcount track in public
+    PUBLIC_SMALL_PLAYCOUNT = "public_small_playcount"
+    # big playcount track in publicJJ:W
+    PUBLIC_BIG_PLAYCOUNT = "public_big_playcount"
 CONCURRENT_TASKS = 1
 IS_NTFT = False
 
 # Asynchronous function to save URL
 async def save_url(url: str, url_type: URLType):
     filename = {
-        URLType.PRIVATE: PRIVATE_URLS_FILENAME,
-        URLType.SMALL_PLAYCOUNT: SMALL_PLAYCOUNT_FILENAME,
-        URLType.NORMAL: NORMAL_URLS_FILENAME
+        URLType.PRIVATE_SMALL_ARTIST: PRIVATE_SMALL_ARTIST_URLS_FILENAME,
+        URLType.PRIVATE_BIG_ARTIST: PRIVATE_BIG_ARTIST_URLS_FILENAME,
+        URLType.PUBLIC_SMALL_PLAYCOUNT: PUBLIC_SMALL_PLAYCOUNT_URLS_FILENAME,
+        URLType.PUBLIC_BIG_PLAYCOUNT: PUBLIC_BIG_PLAYCOUNT_URLS_FILENAME
     }[url_type]
     async with aiofiles.open(os.path.join(LOG_DIR, filename), 'a') as f:
         await f.write(url + '\n')
@@ -49,15 +58,15 @@ async def get_random_url(session):
             if response.status == 200:
                 return url
             if response.status == 403:
-                print("possibly banned, waiting 10 seconds")
-                await asyncio.sleep(10)
+                print("possibly banned, waiting 60 seconds")
+                await asyncio.sleep(60)
 
 # Function to check if the producer is a tiny producer
 async def is_tiny_producer(html_content: str) -> bool:
     match = re.search(r'"playback_count":(\d+)', html_content)
     if match:
         this_playcount = int(match.group(1))
-        return this_playcount < SMALL_PLAYCOUNT_THRESHOLD
+        return this_playcount < PUBLIC_SMALL_PLAYCOUNT_THRESHOLD
     return False
 
 # Function to check if the link is private
@@ -68,19 +77,28 @@ async def is_private_producer(html_content: str) -> bool:
         return sharing == 'private'
     return False
 
+async def is_big_artist(html_content: str) -> bool:
+    match = re.search(r'"followers_count":(\d+)', html_content)
+    if match:
+        followers_count = int(match.group(1))
+        return followers_count > PRIVATE_BIG_ARTIST_THRESHOLD
+    return False
+
 # Asynchronous function to filter URL
 async def filter_url(session, n_url) -> Union[URLType, bool]:
     async with session.get(n_url) as response:
         if response.status != 200:
-            return URLType.NORMAL, True
+            return URLType.PUBLIC_BIG_PLAYCOUNT, True
         html_content = await response.text()
         is_small_playcount = await is_tiny_producer(html_content)
         is_private = await is_private_producer(html_content)
         if is_private:
-            return URLType.PRIVATE, False
+            if await is_big_artist(html_content):
+                return URLType.PRIVATE_BIG_ARTIST, False
+            return URLType.PRIVATE_SMALL_ARTIST, False
         if is_small_playcount:
-            return URLType.SMALL_PLAYCOUNT, False
-        return URLType.NORMAL, False
+            return URLType.PUBLIC_SMALL_PLAYCOUNT, False
+        return URLType.PUBLIC_BIG_PLAYCOUNT, False
 
 # Asynchronous function to notify
 async def ntfy(session, private_url):
@@ -102,17 +120,17 @@ async def bounded_random_url(semaphore, session):
             print(f"URL: {random_url}, Type: {url_type}")
             if not err:
                 await save_url(random_url, url_type)  # Await the async save_url function
-            if url_type == URLType.PRIVATE and IS_NTFT:
-                await ntfy(session, random_url)  # Await the async ntfy function
+            if url_type == URLType.PRIVATE_SMALL_ARTIST or url_type == URLType.PRIVATE_BIG_ARTIST:
+                if IS_NTFT:
+                    await ntfy(session, random_url)  # Await the async ntfy function
 
 def set_concurrent_tasks():
     global CONCURRENT_TASKS
-    print("server mode? (y/n): ")
-    mode = input()
+    mode = input("safe mode? (y/n): ")
     if mode == "y":
-        CONCURRENT_TASKS = 10
-    elif mode == "n":
         CONCURRENT_TASKS = 1
+    elif mode == "n":
+        CONCURRENT_TASKS = 10
     else:
         print("invalid input")
         set_concurrent_tasks()
--- readme.md
+++ readme.md
@@ -2,8 +2,6 @@
 
 ![logs example](_img/ass.png)
 
-mining private & small playcount music on sc based on [SoundCloud Random Link Generator](https://0hy0jy0.codeberg.page/sc_url_gen/@pages/) algorythm by 05
-
 ## setup
 
 ```bash
@@ -14,7 +12,3 @@ python3 steal.py
 ...Ctrl+C to terminate
 cat logs/<private_log_file>.txt
 ```
-
-## ntfy.sh notification
-
-subscribe https://ntfy.sh/sc_private_miner_ntfy to get all private links from all sc_private_miner users!!!
